1d5289371b002961d0642dc45f4e6a2ef3e264f4
inknsharps <nchen25@gmail.com>
2021-04-28 11:18:44 -0400

    add comment model

--- models/Comment.js
+++ models/Comment.js
@@ -2,3 +2,43 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection.js");
 
 class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        },
+        postId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "post",
+                key: "id"
+            }
+        },
+        commentContent: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment"
+    }
+);
+
+module.exports = Comment;
