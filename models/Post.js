7818a66b7c1ab1630c0ce0ffc9b9e8519dca8b71
inknsharps <nchen25@gmail.com>
2021-04-28 11:15:12 -0400

    add initial post model

--- models/Post.js
+++ models/Post.js
@@ -2,3 +2,35 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection.js");
 
 class Post extends Model {};
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        },
+        blogContent: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "post"
+    }
+);
+
+module.exports = Post;
