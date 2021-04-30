6fb434ec083c68c37d9b5c1e0b42c22942a31353
inknsharps <nchen25@gmail.com>
2021-04-29 23:43:30 -0400

    add blog post username to handlebars and routing

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
-const { Post, Comment } = require("../models");
+const { User, Post, Comment } = require("../models");
  
+// We grab only the username and comment content from this?
 router.get("/", async (req, res) => {
     try {
-        const postsData = await Post.findAll();
+        const postsData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"]
+                }
+            ]
+        });
         const posts = postsData.map((post) => post.get({ plain: true }));
-
+        console.log(posts);
         res.render("homepage", {
             posts
         });
