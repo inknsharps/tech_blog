699e6e9a03e5f9c2085fba3c84b21a1f08b30fe8
inknsharps <nchen25@gmail.com>
2021-05-03 21:21:37 -0400

    add signup functionality

---
+++ public/js/signup.js
@@ -0,0 +1,22 @@
+const createAccount = async(event) => {
+    event.preventDefault();
+
+    const username = document.querySelector(".input-username").value.trim();
+    const email = document.querySelector(".input-email").value.trim();
+    const password = document.querySelector(".input-password").value.trim();
+
+    if (username && email && password){
+        const response = await fetch("/api/users/", {
+            method: "POST",
+            body: JSON.stringify({ username, email, password }),
+            headers: { "Content-Type": "application/json" }
+        });
+        if (response.ok) {
+            document.location.replace("/");
+        } else {
+            alert("Failed to create account.");
+        }
+    }
+};
+
+document.querySelector(".signup-form").addEventListener("submit", createAccount);
